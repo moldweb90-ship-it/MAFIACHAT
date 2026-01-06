78e9c1f9d73f146fad1341cd63ed4e81a36bf809
User <user@example.com>
2026-01-06 20:23:49 +0200

    Update server to serve public folder for images

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+// Also serve root files
 app.use(express.static(__dirname));
 
 // Serve index.html for root
